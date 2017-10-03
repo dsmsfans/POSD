b92d9959afd06ad68cc12284901dccfa17943347
dsmsfans <aaa40066@gmail.com>
2017-10-03 16:58:28 +0800

    hw2

---
+++ Number.h
@@ -0,0 +1,38 @@
+#ifndef NUMBER_H
+#define NUMBER_H
+
+#include <string>
+#include "Interface.h"
+#include "Variable.h"
+
+using std::string;
+
+class Number : public Interface
+{
+public:
+  Number (int s):_value(s){}
+
+
+  string symbol(){ return std::to_string(_value);}
+
+  string value(){ return std::to_string(_value);}
+
+  bool match(Interface& interface){ return value() == interface.value(); }
+
+  bool match(Variable& v)
+  {
+    if(v._assignable)
+    {
+      v._value = _value;
+      v._assignable = false;
+      return true;
+    }
+    else
+    {
+      return value() == v.value();
+    }
+  }
+
+  int _value;
+};
+#endif
